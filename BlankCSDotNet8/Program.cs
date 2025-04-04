b19cad8480d032fd41503aab06ec69c0ea55fa05
Sadaf Shaikh <22SShaikh@heckgrammar.co.uk>
2025-04-04 10:13:17 +0100

    DONEEEEE!! in 12min

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
+            DateTime current = DateTime.Now;
+            Console.WriteLine("Please can you input the date and time of the easter break");
+            DateTime target = Convert.ToDateTime(Console.ReadLine());
             
+            TimeSpan difference = target - current;
+            
+            Thread.Sleep(1000);
+            if (Convert.ToInt32(difference) >= 0)
+            {
+                Console.WriteLine(difference.TotalDays + " days, " + difference.TotalHours + " hours, " + difference.Minutes + " minutes, " + difference.Seconds + " seconds.");
+            }
+            else 
+            {
+                Console.WriteLine("Easter break has already begun - therefore the time will be negative");
+                Console.WriteLine(difference.TotalDays + " days, " + difference.TotalHours + " hours, " + difference.Minutes + " minutes, " + difference.Seconds + " seconds.");
+            }
+        
         }
     }
 }
